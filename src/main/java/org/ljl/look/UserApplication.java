4eb52a37848167225c4b86d2920aa54513334cb2
jingling.long <https://mail.aorise.org/#5>
2018-04-15 00:41:59 +0800

    just save

--- src/main/java/org/ljl/look/UserApplication.java
+++ src/main/java/org/ljl/look/UserApplication.java
@@ -3,11 +3,13 @@ package org.ljl.look;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @MapperScan("org.ljl.look.user.mapper")
+@EnableDiscoveryClient
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
